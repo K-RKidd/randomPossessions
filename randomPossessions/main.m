a9764cd35b7a33b03523fbd2eebb3be00c28fdd7
K-RKidd <kr248196@muhlenberg.edu>
2014-01-23 20:25:31 -0500

    Initial Commit

---
+++ randomPossessions/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  randomPossessions
+//
+//  Created by Mac User on 1/23/14.
+//  Copyright (c) 2014 Krystle Kidd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
